fa73ede20c3cb77b36ddcffa1f7891a7b9e48759
detmold <detmold12@gmail.com>
2018-08-26 22:18:00 +0200

    add fixes

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ class Germande {
     public $accumulator = array();
     
     private function checkEdgeCase() {
-        if (empty($this->d)) {
-            $this->output = 0;
-        }
-        else if ($this->o2 == 1) {
+    	if (empty($this->d)) {
+    		$this->output = 0;
+    	}
+         else if (($this->o1 <= 3 && $this->o2 <= 3) || $this->n < 5) {
             $outcome = array_count_values($this->d);
             $this->output = $outcome[1] > $outcome[0] ? 1 : 0;
         }
@@ -20,30 +20,24 @@ class Germande {
 
     private function fillAccumulator() {
         $this->accumulator = array();
-        for ($i=0; $i<$this->n+$this->o2-1; $i++) {
-            if ($i==0) {
-                $this->accumulator[$i] = $this->d[$i];
-            } else {
-                $k = $i % $this->n;
-                $k1 = $i > $this->n ? $k - 1 : $i-1;
-                $this->accumulator[$i] = $this->accumulator[$k1] + $this->d[$k];
-            }
+        for ($i=0; $i<=$this->n+$this->o2-1; $i++) {
+	     $k = $i % $this->n;
+	     $k1 = $i > $this->n && k != 0 ? $k-1 : $i-1;
+	     $this->accumulator[$i] = $this->accumulator[$k1] + $this->d[$k];
         } 
     }
 
     private function process() {
         $this->output = 0;
         $halfDistrict = ceil($this->o2/2);
-        for ($i=0; $i<$this->o1; $i++) {  //petla po dystryktach
+        
+        for ($i=0; $i<=$this->o2; $i++) {  //petla po dystryktach  
             $results = array();
             
-            for ($k=$this->o2; $k<=$this->n; $k+=$this->o2) {
-                if ($k == $this->o2 && $i == 0) {
-                    $licznik = 0;
-                } else {
-                    $licznik = $this->accumulator[$k+$i-$this->o2-1];
-                }
-                $results[] = $this->accumulator[$k+$i-1] - $licznik >= $halfDistrict ? 1 : 0;
+            for ($k=$this->o2; $k<$this->n+$this->o2; $k+=$this->o2) {
+                $licznik1 = $k+$i-1;
+                $licznik = $this->accumulator[$k+$i-$this->o2-1];
+                $results[] = $this->accumulator[$licznik1] - $licznik >= $halfDistrict ? 1 : 0;
             }
             // w results powinnismy miec 0 lub 1 per dystrykt
             $outcome = array_count_values($results);
@@ -55,13 +49,13 @@ class Germande {
     }
 
     public function init() {
-        $this->test = trim(stream_get_line(STDIN, 100, PHP_EOL));
+        $this->test = trim(stream_get_line(STDIN, 10000, PHP_EOL));
         for ($i=0; $i<$this->test; $i++) {
             $this->output = false;
-            $states = explode(' ', stream_get_line(STDIN, 100000, PHP_EOL));
+            $states = explode(' ', trim(stream_get_line(STDIN, 10000000, PHP_EOL)));
             $this->o1 = $states[0];
             $this->o2 = $states[1];
-            $this->d = explode(' ', stream_get_line(STDIN, 1000000, PHP_EOL));
+            $this->d = explode(' ', trim(stream_get_line(STDIN, 10000000, PHP_EOL)));
             $this->n = count($this->d);
 
             $this->checkEdgeCase();
